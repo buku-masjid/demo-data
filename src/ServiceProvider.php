eca452e9af60824aa1ea046e175c7fcec4ddda83
Nafies Luthfi <nafiesl@gmail.com>
2023-09-29 10:11:04 +0800

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BukuMasjid\DemoData;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
